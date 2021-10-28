2eeed87186822190d89dee170e8482c7477e6869
Vlad <levitinvlad@hotmail.com>
2021-10-28 03:01:23 +0200

    first notebook

---
+++ evaluation_metric.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+def root_mean_squared_log_error(y_true, y_pred):
+    # Alternatively: sklearn.metrics.mean_squared_log_error(y_true, y_pred) ** 0.5
+    assert (y_true >= 0).all() 
+    assert (y_pred >= 0).all()
+    log_error = np.log1p(y_pred) - np.log1p(y_true)  # Note: log1p(x) = log(1 + x)
+    return np.mean(log_error ** 2) ** 0.5
